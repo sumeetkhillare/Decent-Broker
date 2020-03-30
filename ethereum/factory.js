b8fb51e4cbf565bfcd80ea184daf1e0238e3250b
sanket pathak <sanketpathak64@gmail.com>
2020-03-31 00:39:19 +0530

    modified factory

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,8 +1,8 @@
 import web3 from './web3';
-import CampaignFactory from './build/ContractDeployer.json';
+import ContractDeployer from './build/ContractDeployer.json';
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
+  JSON.parse(ContractDeployer.interface),
   '0xfbD6099696F6f2E76F8CA2014143a8Cf9c224D8E'
 );
 export default instance;
